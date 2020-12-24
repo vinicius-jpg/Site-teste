5fce53ec057d9eb04103a00df1111b06f49dfc32
vinicius-jpg <71409236+vinicius-jpg@users.noreply.github.com>
2020-12-24 10:36:43 -0300

    Adicionei arquivos

---
+++ index.js
@@ -0,0 +1,14 @@
+var vel = 50
+    console.log(`A velocidade do carro e ${vel}`)
+        if (vel >=60) { //condicao simples
+            console.log('ultrapassou os limites')
+        }
+        console.log('dirija usando cinto')
+
+var pais = 'Brazil'
+    console.log(`vivendo em ${pais}`)
+        if (pais == 'Brazil') {
+            console.log('Voce e Brasileiro')
+        } else {
+            console.log('Voce e gringo')
+        }
